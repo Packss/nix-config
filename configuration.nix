d5655330f89e6d9a65c49da1f93d2cdb1621dcab
Packss <enzoana@hotmail.com>
2026-04-03 22:58:03 -0300

    before update

--- configuration.nix
+++ configuration.nix
@@ -47,15 +47,15 @@ in
     modesetting.enable = true;
     powerManagement.enable = true;
     powerManagement.finegrained = true;
-    open = true;
+    open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
       version = "595.58.03";
       sha256_64bit = "sha256-jA1Plnt5MsSrVxQnKu6BAzkrCnAskq+lVRdtNiBYKfk=";
-      sha256_aarch64 = "sha256-jl6lQWsgF6ya22sAhYPpERJ9r+wjnWzbGnINDpUMzsk=";
-      openSha256 = "sha256-uqNfImwTKhK8gncUdP1TPp0D6Gog4MSeIJMZQiJWDoE=";
+      sha256_aarch64 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      openSha256 = "sha256-6LvJyT0cMXGS290Dh8hd9rc+nYZqBzDIlItOFk8S4n8=";
       settingsSha256 = "sha256-2vLF5Evl2D6tRQJo0uUyY3tpWqjvJQ0/Rpxan3NOD3c=";
-      persistencedSha256 = "sha256-5FoeUaRRMBIPEWGy4Uo0Aho39KXmjzQsuAD9m/XkNpA=";
+      persistencedSha256 = "sha256-AtjM/ml/ngZil8DMYNH+P111ohuk9mWw5t4z7CHjPWw=";
     };
   };
 
@@ -124,6 +124,7 @@ in
   ];
 
   services.fstrim.enable = true;
+  services.udisks2.enable = true;
   hardware.block.scheduler."nvme[0-9]*" = "kyber";
   systemd.tmpfiles.rules = [ "d /mnt/games 0755 enzo users -" ];
 
@@ -188,6 +189,7 @@ in
       "libvirtd"
       "kvm"
       "video"
+      "render"
     ];
   };
 
